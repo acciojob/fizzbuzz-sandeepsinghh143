f44c0ed23d3212433a538330df2dfffff214fa5c
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-07 10:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //your JS code here. If required.
 function fizzBuzz() {
+    let op="";
       for (let i = 1; i <= 20; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-          alert("FizzBuzz");
-        } else if (i % 3 === 0) {
-          alert("Fizz");
-        } else if (i % 5 === 0) {
-          alert("Buzz");
-        } else {
-          alert(i.toString());
+          op+="FizzBuzz";
+        } 
+        else if (i % 3 === 0) {
+          op+="Fizz";
+        }
+        else if (i % 5 === 0) {
+          op+="Buzz";
+        } 
+        else {
+          op+=i;
         }
       }
+      alert(op);
     }
